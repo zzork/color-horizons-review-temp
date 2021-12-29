01109094d5970dfdd2f1d2988c4b2cb3e4c735a2
Jon Lervold <jonlervold@gmail.com>
2021-12-29 06:41:00 -0700

    got lms readout working

--- src/components/CalcLTReadout2.js
+++ src/components/CalcLTReadout2.js
@@ -23,6 +23,39 @@ export const CalcLTReadout2 = ({ numerator, denominator, noteAmount }) => {
   sortedUnique.sort((a, b) => a - b);
   sortedUnique.reverse();
 
+  ///////
+
+  // take values of uniques and map to LMS
+  let lSize = 0;
+  let mSize = 0;
+  let sSize = 0;
+  if (sortedUnique.length === 2) {
+    lSize = sortedUnique[0];
+    sSize = sortedUnique[1];
+  }
+  if (sortedUnique.length === 3) {
+    lSize = sortedUnique[0];
+    mSize = sortedUnique[1];
+    sSize = sortedUnique[2];
+  }
+
+  const valueToLms = (value) => {
+    if (value === lSize) {
+      return "L";
+    }
+    if (value === mSize) {
+      return "m";
+    }
+    if (value === sSize) {
+      return "s";
+    }
+  };
+
+  // take mapped LMS and create new list from previous values
+  let lmsList = stepDifferences.map((value) => valueToLms(value.toFixed(5)));
+
+  ///////
+
   // uniques display logic
   let uniquesDisplay = null;
   if (sortedUnique.length < 2) {
@@ -64,6 +97,7 @@ export const CalcLTReadout2 = ({ numerator, denominator, noteAmount }) => {
 
   return (
     <div>
+      {lmsList}
       <p>Step Sizes</p>
       {uniquesDisplay} <br />
       {finalTable}

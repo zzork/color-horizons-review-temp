5c09c5db0e77ac5676c786c72735c9b7eb3a2600
Jon Lervold <jonlervold@gmail.com>
2022-01-07 14:46:33 -0700

    removed some now-unnecessary imports

--- src/apps/Calculators/LTByRatio/util/convertLTInputToModesObjects.js
+++ src/apps/Calculators/LTByRatio/util/convertLTInputToModesObjects.js
@@ -1,8 +1,6 @@
 import getStepDifferences from "../../../../util/getStepDifferences";
 import getUniqueSteps from "../../../../util/getUniqueSteps";
-import convertFractionModeToCentsScale from "./convertFractionModeToCentsScale";
 import getAllLTModesAsCents from "./getAllLTModesAsCents";
-import getAllLTModesAsFractions from "./getAllLTModesAsFractions";
 import getLMSList from "./getLMSList";
 
 const convertLTInputToModesObjects = (reducedFraction, noteTotal) => {
@@ -21,19 +19,6 @@ const convertLTInputToModesObjects = (reducedFraction, noteTotal) => {
       lmsList,
     });
   }
-
-  // for (let i = 0; i < allLTModesAsFractions.length; i++) {
-  //   let mode = convertFractionModeToCentsScale(allLTModesAsFractions[i]);
-  //   let stepDifferences = getStepDifferences(mode);
-  //   let sortedUnique = getUniqueSteps(stepDifferences);
-  //   let lmsList = getLMSList(stepDifferences, sortedUnique);
-  //   allLTObjects.push({
-  //     fractionScale: allLTModesAsFractions[i],
-  //     mode,
-  //     stepDifferences,
-  //     lmsList,
-  //   });
-  // }
 
   return allLTObjects;
 };

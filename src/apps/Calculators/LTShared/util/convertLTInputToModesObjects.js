bc386493076b7cc1e2c7903fbf62a3d7d19bef92
Jon Lervold <jonlervold@gmail.com>
2022-01-12 07:02:59 -0700

    hell yes got the duplicate modes to not display

--- src/apps/Calculators/LTShared/util/convertLTInputToModesObjects.js
+++ src/apps/Calculators/LTShared/util/convertLTInputToModesObjects.js
@@ -6,16 +6,22 @@ const convertLTInputToModesObjects = (scale, lmsIn) => {
   const allLTModesAsCents = getAllLTModesAsCents(scale);
 
   let allLTObjects = [];
+  let lmsComparisonList = [];
 
   for (let i = 0; i < scale.length - 1; i++) {
     let mode = allLTModesAsCents[i];
     let stepDifferences = getStepDifferences(mode);
     let lmsList = getLMSList(stepDifferences, lmsIn);
-    allLTObjects.push({
-      mode,
-      stepDifferences,
-      lmsList,
-    });
+
+    if (!lmsComparisonList.includes(lmsList.join(""))) {
+      allLTObjects.push({
+        mode,
+        stepDifferences,
+        lmsList,
+      });
+    }
+
+    lmsComparisonList.push(lmsList.join(""));
   }
 
   return allLTObjects;
